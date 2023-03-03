e5b497a984e684afdff276c87192d1279fc2bff4
Samira Tahirou <tahirousamira@gmail.com>
2023-03-03 19:27:20 +0100

    Debugging

--- src/components/BannerImg.jsx
+++ src/components/BannerImg.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import "./BannerImg.scss"
 
 export function BannerImg(props) {
-  // const imageUrl = props.imageUrl ? props.imageUrl : "https://picsum.photos/1920/1080"
   const { pictures } = props
   // Show picture
   const [currentPicture, setCurrentPicture] = useState(0)
@@ -12,11 +11,11 @@ export function BannerImg(props) {
   }
   // Move Next picture
   const moveToNext = () => {
-    setCurrentPicture((currentPicture + 1) % pictures.length) // % pictures.length c'est un modulo. en gros quand je lance ma function qui est = à 0, puis 1, 2.. et je lui dit de ne jamais dépasser pictures.length
+    setCurrentPicture((currentPicture + 1) % pictures.length)
   }
   // Move Previous picture
   const moveToPrevious = () => {
-    const newCurrentPicture = currentPicture - 1 // Au début notre current picture est = à 0, et je lui dis, que le nouveau sera -1, mais attention, si c'est klkchose qui est inférieur à O, il faut que notre nouvel index sera "pictures.length -1" et return, tu t'arrêtes. Et sinon tu me fait un (currentPicture - 1)
+    const newCurrentPicture = currentPicture - 1
     if (newCurrentPicture < 0) {
       setCurrentPicture(pictures.length - 1)
       return
@@ -30,7 +29,7 @@ export function BannerImg(props) {
   const getCarouselOrDefaultImage = () => {
     if (!arePicturesAvalable()) {
       return <img src="./images/about-img.png" className="show" alt="" />
-    } // s'il n'y a pas pictures ou que le pictures.length n'est pas bon dans ce là tu nous return une image, et sinon tu nous renvoie pictures.map...
+    }
     return pictures.map((pic, i) => (
       <img key={pic} src={pic} alt="" className={getClassName(i)} />
     ))
